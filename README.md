a5d38c5a2e3c438871628e65c4b2b74d1aed1416
Timothy Foster <tf.auroratide@gmail.com>
2022-01-31 10:03:52 -0600

    A hello world app

---
+++ README.md
@@ -0,0 +1,10 @@
+Goals:
+
+* Tap a die repeatedly for it to accumulate a sum
+* Accessibility of course
+
+Stack:
+
+* React
+* CSS Modules
+* Jest with Testing Library
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom'
+import { Greeting } from './Greeting'
+
+ReactDOM.render(
+    <Greeting text="world" />,
+    document.getElementById('root')
+)
