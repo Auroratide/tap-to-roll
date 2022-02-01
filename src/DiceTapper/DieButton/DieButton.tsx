5c25a0a20256ce6f3d2db15b12e43ca6f5cb9dad
Timothy Foster <tf.auroratide@gmail.com>
2022-02-01 15:17:07 -0600

    Pulse animation when pressing a die for feedback

--- src/DiceTapper/DieButton/DieButton.tsx
+++ src/DiceTapper/DieButton/DieButton.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react'
 import * as classes from './DieButton.module.css'
 
 export type DieButtonProps = {
@@ -6,9 +7,29 @@ export type DieButtonProps = {
     label: string,
 }
 
-export const DieButton = ({ onClick, icon, label }: DieButtonProps) => (
-    <button className={classes['die-button']} onClick={onClick}>
-        <span className={classes.icon}>{icon}</span>
-        <span className={classes.label}>{label}</span>
-    </button>
-)
+export const DieButton = ({ onClick, icon, label }: DieButtonProps) => {
+    const pulse = useRef<HTMLElement>()
+
+    const animatePulse = () => {
+        let elem = pulse.current
+        if (elem !== undefined) {
+            elem.style.animation = 'none'
+            let _ = elem.offsetHeight // trigger reflow
+            elem.style.animation = null
+            elem.style.animationPlayState = 'running'
+        }
+    }
+
+    const handleClick = () => {
+        onClick()
+        animatePulse()
+    }
+
+    return (
+        <button className={classes['die-button']} onClick={handleClick}>
+            <span className={classes.icon}>{icon}</span>
+            <span className={classes.label}>{label}</span>
+            <span ref={pulse} className={classes.pulse}>{icon}</span>
+        </button>
+    )
+}
