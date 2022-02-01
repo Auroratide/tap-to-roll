3c067e329ae9b247b914ea72200f8601f868ebe2
Timothy Foster <tf.auroratide@gmail.com>
2022-02-01 10:14:55 -0600

    Abbreviate the footer

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import * as classes from './Footer.module.css'
 
 export const Footer = () => (
     <footer className={classes.footer}>
-        <p>Made by <a href="https://auroratide.com">Timothy Foster</a>, a human that plays DnD sometimes</p>
-        <p>This app is <a href="/">open source</a>!</p>
+        <p>Made by <a href="https://auroratide.com">Auroratide</a></p>
+        <p><a href="/">Github</a></p>
     </footer>
 )
