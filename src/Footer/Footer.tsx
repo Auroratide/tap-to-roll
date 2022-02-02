d75485778a1e5b98c995d3c38b2327cfbed87564
Timothy Foster <tf.auroratide@gmail.com>
2022-02-01 21:18:13 -0600

    Github link

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -3,6 +3,6 @@ import * as classes from './Footer.module.css'
 export const Footer = () => (
     <footer className={classes.footer}>
         <p>Made by <a href="https://auroratide.com">Auroratide</a></p>
-        <p><a href="/">Github</a></p>
+        <p><a href="https://github.com/Auroratide/tap-to-roll">Github</a></p>
     </footer>
 )
