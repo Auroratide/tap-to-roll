b63bec8968827d15e547e219ba8508c7c520c249
Timothy Foster <tf.auroratide@gmail.com>
2022-02-02 11:48:56 -0600

    Include a service worker to cache files

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,11 @@
 import ReactDOM from 'react-dom'
 import { DiceTapper } from './DiceTapper'
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register(
+        new URL('service-worker.ts', import.meta.url),
+        { type: 'module' }
+    )
+}
+
 ReactDOM.render(<DiceTapper />, document.getElementById('app'))
---
+++ src/service-worker.ts
@@ -0,0 +1,20 @@
+/// <reference lib="WebWorker" />
+import { manifest, version } from '@parcel/service-worker'
+declare const self: ServiceWorkerGlobalScope
+
+const cacheName = `taptoroll-${version}`
+
+const install = async () => {
+    const cache = await caches.open(cacheName)
+    await cache.addAll(manifest)
+}
+self.addEventListener('install', (e) => e.waitUntil(install()))
+
+const activate = async () => {
+    const keys = await caches.keys()
+    await Promise.all(keys.map(key => {
+        if (key === cacheName) return;
+        return caches.delete(key)
+    }))
+}
+self.addEventListener('activate', (e) => e.waitUntil(activate()))
