197a1b99f2113ef2840b864b4386fe37a783e04b
Timothy Foster <tf.auroratide@gmail.com>
2022-02-01 19:05:22 -0600

    Provide an example for clarity

--- src/DiceTapper/Instructions/Instructions.tsx
+++ src/DiceTapper/Instructions/Instructions.tsx
@@ -5,7 +5,7 @@ export const Instructions = () => (
         <h2 className={classes.title}>Instructions</h2>
         <ol className={classes.list}>
             <li>Tap a die to roll it.</li>
-            <li>Tap one or more dice in quick succession to aggregate results.</li>
+            <li>Tap one or more dice in quick succession to aggregate results. For example, to roll <b>8d6</b>, tap "d6" eight times.</li>
             <li>After some time, the series ends; tapping a die will start a new series.</li>
             {/* <li>Or, you can press "Clear".</li> */}
         </ol>
