a7b0cdc1505da2f242e6ff6e5e0b6c2519ed8945
Timothy Foster <tf.auroratide@gmail.com>
2022-02-02 08:59:37 -0600

    A simple parchment background effect

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,16 @@
 import { DiceTapper } from '../DiceTapper'
 import { Footer } from '../Footer'
+import { ParchmentBackground } from '../ParchmentBackground'
 import * as classes from './App.module.css'
 
 export const App = () => (
-    <div className={classes.container}>
-        <main>
-            <h1 className={classes.title}>Tap to Roll</h1>
-            <DiceTapper />
-        </main>
-        <Footer />
-    </div>
+    <ParchmentBackground>
+        <div className={classes.container}>
+            <main>
+                <h1 className={classes.title}>Tap to Roll</h1>
+                <DiceTapper />
+            </main>
+            <Footer />
+        </div>
+    </ParchmentBackground>
 )
